c2026656a53dc177cd99813dabc9fa40125e7bcb
thientantran <thientantm@gmail.com>
2023-06-13 10:07:13 +0700

    Create database and connect database from server
    sequelize db:create
    yarn dev

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const path = require("path")
+const {sequelize} = require("./models")
 const app =express()
 
 app.use(express.json())
@@ -7,6 +8,11 @@ app.use(express.json())
 const publicPathDirectory  = path.join(__dirname, "./public")
 app.use(express.static(publicPathDirectory))
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+app.listen(3000, async() => {
+    try {
+        await sequelize.authenticate()
+        console.log("Connected")
+    } catch (error) {
+        console.error("Conenct Fail")
+    }
 })
