39ddc4280d729c41a3466e574eb9352ae1ffcf2e
thientantran <thientantm@gmail.com>
2023-06-17 15:11:19 +0700

    create model for ticket
    yarn sequelize model:generate --name ticket --attributes id:integer

---
+++ models/ticket.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ticket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ticket.init({
+    id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ticket',
+  });
+  return ticket;
+};
