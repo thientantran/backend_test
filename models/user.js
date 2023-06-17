a56c96e311547753e94e7851e9ac581aaf0942fe
thientantran <thientantm@gmail.com>
2023-06-17 15:45:49 +0700

    Create fake seed for data
    yarn sequelize seed:generate --name create-user-fake
    fixup
    
    
    fixup

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(ticket) {
+    static associate({ticket}) {
       // define association here
       this.hasMany(ticket, {foreignKey: "user_id"})
     }
